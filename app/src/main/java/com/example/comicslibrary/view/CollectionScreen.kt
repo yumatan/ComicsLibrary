e101ea7276bf7574d9c6d8a8204eadeac4ef5fbc
yumatan <yuma.tanaka1129@gmail.com>
2024-02-25 01:00:57 +0900

    add: empty views

---
+++ app/src/main/java/com/example/comicslibrary/view/CollectionScreen.kt
@@ -0,0 +1,9 @@
+package com.example.comicslibrary.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CollectionScreen(){
+    Text(text = "Collection screen")
+}
