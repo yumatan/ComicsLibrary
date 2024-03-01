a8e14e9fb4a3a0e5f9a0cbbedcb2bffbfb71ada6
yumatan <yuma.tanaka1129@gmail.com>
2024-03-01 23:58:03 +0900

    fix: generate hash process

--- app/src/main/java/com/example/comicslibrary/Util.kt
+++ app/src/main/java/com/example/comicslibrary/Util.kt
@@ -12,7 +12,7 @@ import java.math.BigInteger
 import java.security.MessageDigest
 
 fun getHash(timestamp: String, publicKey: String, privateKey: String): String {
-    val hashStr = timestamp + publicKey + privateKey
+    val hashStr = timestamp + privateKey + publicKey
     val md = MessageDigest.getInstance("MD5")
     return BigInteger(1, md.digest(hashStr.toByteArray())).toString(16).padStart(32, '0')
 }
